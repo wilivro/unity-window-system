fe5ddf0d21e0d65f4806b7f2c4fad18f5fc36cb3
kalmik <sergio.kalmik@gmail.com>
2016-03-24 21:01:19 -0300

    QuestLog

---
+++ Assets/Resources/Items/Scripts/Item.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Xml;
+using System.Xml.Serialization;
+
+public class Item
+{
+	public string name;
+
+	public string description;
+
+	private string icon;
+
+	private string image;
+
+	public int qtd;
+}
